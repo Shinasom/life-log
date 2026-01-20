889af0e59df8a5d86099bffd25b30d91d106addb
Shinas Om <shinasom@gmail.com>
2026-01-21 03:09:35 +0530

    Add STATIC_ROOT for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,6 +13,8 @@ import dj_database_url
 import os
 from pathlib import Path
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
